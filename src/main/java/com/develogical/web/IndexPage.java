2065e661904b6ecc96d78e7e67559968ff55c939
Shannon Ding <shannon_ding@yahoo.com>
2022-09-22 10:21:26 -0400

    okay

--- src/main/java/com/develogical/web/IndexPage.java
+++ src/main/java/com/develogical/web/IndexPage.java
@@ -7,7 +7,7 @@ public class IndexPage extends HtmlPage {
     @Override
     protected void writeContentTo(PrintWriter writer) {
         writer.println(
-                "<h1>You're welcome!</h1>" +
+                "<h1>You are welcome!</h1>" +
                 "<p>Enter your query in the box below: " +
                   "<form><input type=\"text\" name=\"q\" />" +
                     "<input type=\"submit\">" +
